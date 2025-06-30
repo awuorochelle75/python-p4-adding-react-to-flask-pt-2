a3a1dbed6a559b87d7124d012966e658b424f796
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-30 21:45:38 +0300

    creates and modifies files

--- server/models.py
+++ server/models.py
@@ -9,5 +9,11 @@ class Movie(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title
+        }
+
     def __repr__(self):
         return f'<Movie {self.title}>'
